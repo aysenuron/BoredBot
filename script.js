a7eaa8422589c94dbd3c58b032261539df1c5ec4
aysenuron <onaranays@gmail.com>
2025-03-01 10:21:05 +0300

    Change button's color with each click.

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const btn = document.querySelector("button");
 const message = document.querySelector("p");
 
+const colors = ["red", "blue", "orange", "green", "lightblue", "blueviolet", "chartreuse", "cadetblue", "fuchsia", "greenyellow", "hotpink", "coral", "cornflowerblue"]
+
+function getRandomNumber() {
+    let randomNumber;
+    randomNumber = Math.floor(Math.random() * (colors.length - 0 + 1) + 0);
+    return randomNumber;
+  }
+
 btn.addEventListener("click", () => {
     fetch("https://apis.scrimba.com/bored/api/activity")
         .then (responsive => responsive.json())
-        .then (data => message.textContent = data.activity);
+        .then (data => {
+            message.textContent = data.activity;
+            btn.style.backgroundColor = colors[getRandomNumber()];
+        });
 });
